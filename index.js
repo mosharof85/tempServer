07aaa310e4280ca6a836c81f198958608e8e414c
mosharof85 <68509780+mosharof85@users.noreply.github.com>
2021-04-02 02:56:43 +0600

    'update'

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express')
 const cors = require('cors');
 const bodyParser = require('body-parser');
 const MongoClient = require('mongodb').MongoClient;
+const ObjectID = require('mongodb').ObjectID;
 require('dotenv').config();
 
 
@@ -21,6 +22,37 @@ client.connect(err => {
     console.log('Connection Error', err);
 
     const products = client.db(`${process.env.DB_NAME}`).collection("products");
+    const orders = client.db(`${process.env.DB_NAME}`).collection("orders");
+
+    app.post('/addOrder', (req, res)=>{
+        orders.insertOne(req.body)
+            .then(result=>{
+                res.send(result.insertedCount > 0);
+            })
+    })
+
+    app.get('/orders/:email', (req, res)=>{
+
+        const email = (req.params.email);
+        const ids = [];
+
+        orders.find({email: email})
+            .toArray((err, orders) =>{
+                orders.forEach(item=>{
+                    ids.push(ObjectID(item.productID))
+                })
+
+                products.find({
+                    _id: {
+                        $in: ids
+                    }
+                })
+                    .toArray((err, products) =>{
+                        res.send(products);
+                })
+
+            })
+    })
 
     app.post('/addProduct', (req, res)=>{
         products.insertOne(req.body)
@@ -29,6 +61,37 @@ client.connect(err => {
             })
     })
 
+    app.get('/products', (req, res)=>{
+        products.find()
+            .toArray((err, items) =>{
+                res.send(items);
+            })
+    })
+
+    app.get('/product/:id', (req, res)=>{
+
+        const id = ObjectID(req.params.id);
+
+        products.find({_id: id})
+            .toArray((err, items) =>{
+                res.send(items[0]);
+            })
+    })
+
+    app.delete('/deleteProduct/:id', (req, res)=>{
+        const id = ObjectID(req.params.id);
+
+        products.findOneAndDelete({_id: id}, (err, result)=>{
+            console.log(result)
+            if (err)
+            {
+                res.send(err)
+            }
+            else {
+                res.send(result)
+            }
+        })
+    })
 });
 
 app.get('/', (req, res) => {
